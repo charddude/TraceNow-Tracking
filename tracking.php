eda3e5d92d3ff9e00041671db30b30b0cb4cdc11
Richard King <titandesignuk@gmail.com>
2014-08-08 10:56:05 +0100

    Update tracking.php

--- tracking.php
+++ tracking.php
@@ -11,14 +11,10 @@
  License URI: http://www.opensource.org/licenses/gpl-license.php 
  */
  
-// Create tracking nonce
- 
- function tracking_nonce(){
- 		 $nonce = wp_create_nonce( 'tracking_function' );
- }
+
  
  // enqueue and localise scripts
- 
+
  wp_enqueue_script( 'my-ajax-handle', plugin_dir_url( __FILE__ ) . 'ajax.js', array( 'jquery' ) );
  wp_enqueue_style( 'my-ajax-handle', plugin_dir_url( __FILE__ ) . 'style.css' );
  wp_localize_script( 'my-ajax-handle', 'the_ajax_script', array( 'ajaxurl' => admin_url( 'admin-ajax.php' ) ) );
@@ -30,34 +26,66 @@
  add_action( 'wp_ajax_nopriv_the_ajax_hook', 'tracking_function' ); // need this to serve non logged in users
  
  
- // THE FUNCTION
+ // THE CONSIGNMENT FUNCTION
+ 
+ $memberGuid = array('memberGuid' => 'd5dbffb4-0336-44bf-b72c-00fb9aaac759');
+ $requiredFields = array('consignmentNumber');
+ $formData = getFormData($requiredFields);
+
+ function getFormData($requiredFields){
+       $formData = array();
+       $formData['valid'] = true;
+       $formData['fields'] = array();
+       $formData['notValidFields'] = array();
+
+       for($a = 0; $a < count($requiredFields); $a++){
+               $field = $requiredFields[$a];
+               if(isset($_POST[$field])){
+                       $value = $_POST[$field];
+                       if(empty($value)){
+                               $formData['valid'] = false;
+                               $formData['notValidFields'][] = $field;
+                       }else{
+                               $formData['fields'][$field] = $value;
+                       }
+               }else{
+                       $formData['valid'] = false;
+                       $formData['notValidFields'][] = $field;
+               }
+       }
+       return $formData;
+ }
+ 
+ 
+ // THE TRACKING FUNCTION
  
  function tracking_function(){ 
- 
+
  if( !class_exists( 'WP_Http' ) )
     include_once( ABSPATH . WPINC. '/class-http.php' );
-		$host = $_SERVER['HTTP_HOST'] . $url;
-		$consignmentNumber = $_GET['name'];
- 		$memberGuid = $_POST['d5dbffb4-0336-44bf-b72c-00fb9aaac759'];
-		$response = wp_remote_post( $url = ('http://services.tracenow.net/TraceNowAccess.asmx/GetConsignment'), array(
+ 
+		$url = ('http://services.tracenow.net/TraceNowAccess.asmx/GetConsignment');
+		
+		$response = wp_remote_post( $url, $formData,  array(
 											'method' => 'POST',
 											'timeout' => 45,
 											'redirection' => 5,
 											'httpversion' => '1.1',
+											'content-type' => 'application/x-www-form-urlencoded',
 											'blocking' => true,
-											'headers' => array(),
-											'body' => array( 'consignmentNumber' => $consignmentNumber, 'memberGuid' => $memberGuid ),
-											'cookies' => array()
+											'body' => array(),
     									   )
 									);
 		if ( is_wp_error( $response ) ) {
    				$error_message = $response->get_error_message();
   					 echo "Something went wrong: $error_message";
 				} else {
-						echo 'Consignment details: ' .  urldecode( $_GET[ 'body' . data ] );// this is passed back to the javascript function
+						echo '<pre>', 'Consignment details: ', print_r($response), '</pre>';// this is passed back to the javascript function
+						die();
  				}
 				
- }
+}
+
 				
  // ADD Tracking FORM TO THE PAGE
  
@@ -66,8 +94,8 @@
  <form id="theForm">
  <input id="consignmentNumber" name="consignmentNumber" value="Consignment No" type="text" />
  <input name="action" type="hidden" value="the_ajax_hook" /> <!-- this puts the action the_ajax_hook into the serialized form -->
- <input name="memberGuid" type="hidden" value="" />
- <input id="submit_button" value ="Track" type="button" onClick="submit_me();" />
+ <input id="memberGuid" name="memberGuid" type="hidden" />
+ <input id="submit_button" value="Track" type="button" onClick="submit_me();" />
  </form>
  <div id="response_area">
  	Your tracking details will appear here
@@ -76,3 +104,4 @@
  }
  add_shortcode("tn_ajax_frontend", "tracenow_frontend");
  ?>
+
