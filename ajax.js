ad66c7bdcf41556418eaa1601ea32031d8b8eb6d
Richard King <titandesignuk@gmail.com>
2014-09-08 08:14:47 +0100

    Updated ajax.js

--- ajax.js
+++ ajax.js
@@ -1,10 +1,21 @@
 // Tracking JQuery call
- 
+
 function submit_me(){
-		jQuery.post(the_ajax_script.ajaxurl, jQuery("#theForm").serialize()
+		jQuery.post(the_ajax_script.ajaxurl, jQuery('#theForm').serialize()
 		,
 		function(response_from_tracking_function){
 			jQuery("#response_area").html(response_from_tracking_function);
 			}
 		);
+		
+		jQuery('#theForm').find('input[type=text]').val('Consignment No');
+		
+
+		/*var img = $('<img id="dynamic">'); //Equivalent: $(document.createElement('img'))
+		img.attr('deliverysignature', responseObject.imgurl);
+		img.appendTo('#imagediv');*/
+		
+		jQuery("#imagediv").append("<img id='theImg' src='http://resources.tracenow.net/" + consignment.find("deliverysignature").text() + "'/>");
+		
+		
 }
